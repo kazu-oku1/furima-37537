7f227f7ab30b2a957d4ebf156862ccde24451605
kazu-oku1 <hawai.waikiki.oahu7@gmail.com>
2022-05-25 21:58:17 +0900

    単体テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "英数字をそれぞれ含めてください" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'に英数字をそれぞれ含めてください' }
   validates :nickname, presence: true
-  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角を入力してください' }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角を入力してください' }
-  validates :family_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを入力してください' }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを入力してください'}
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'に全角を入力してください' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'に全角を入力してください' }
+  validates :family_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'に全角カナを入力してください' }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'に全角カナを入力してください' }
   validates :birth_day, presence: true
 
-  #has_many :items
-  #has_many :purchases
+  # has_many :items
+  # has_many :purchases
 end
