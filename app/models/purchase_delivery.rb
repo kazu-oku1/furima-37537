90146af28bacb78b8de0774977a73febcbb268e3
kazu-oku1 <hawai.waikiki.oahu7@gmail.com>
2022-06-18 21:52:26 +0900

    レビュー修正2

--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -3,14 +3,14 @@ class PurchaseDelivery
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :token, presence: true
+    validates :token
     validates :item_id
     validates :user_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. is too short' }
+    validates :city, presence: true
+    validates :address, presence: true
   end
-  validates :city, presence: true
-  validates :address, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
